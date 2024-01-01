2c9ec3e9d0722c99b667a5ab2819a1f2b94c1fb7
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-01-02 00:01:45 +0800

    ja practice

---
+++ practice.js
@@ -0,0 +1,12 @@
+// variable(var)
+var myName = "Shamim";
+console.log(myName)
+
+
+
+var strType = "I am Shamim";
+console.log(typeof strType)
+
+var numType = 8639486934867;
+console.log(typeof numType)
+
